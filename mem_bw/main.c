1b34ee85ce22f090f2424a7672426d2eef972aab
yosshin <koichi.yoshida@gmail.com>
2023-02-13 06:09:26 +0900

    Record the CRC32 of the executable file.

--- mem_bw/main.c
+++ mem_bw/main.c
@@ -9,6 +9,7 @@
 #include <iocslib.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stat.h>
 #include <string.h>
 #include <basic0.h>
 
@@ -92,6 +93,108 @@ static const char *(s_memcpyMethodDescriptions[]) = {
 	"memcpyByDma Limited-rate BwRatio50%%%% BurstTime16 LiteCsrPolling\n",
 	"memcpyByDma Limited-rate BwRatio50%%%% BurstTime16 FullCsrPolling\n",
 };
+
+/*
+	ログ記録まわり
+	ディスクへの書き込み頻度を減らすため、バッファに溜めて一気に書き出す。
+*/
+static char s_logBuffer[256 * 1024] = {0};
+size_t s_logBufferIndex = 0;
+static bool printLogFile(
+	const char *string
+){
+	printf("%s", string);
+	size_t len = strlen(string) + 1;						/* 終端 \0 を含ませた長さ */
+	if (s_logBufferIndex + len > sizeof(s_logBuffer)) {
+		return false;
+	}
+	memcpy(&s_logBuffer[s_logBufferIndex], string, len);	/* 終端 \0 もコピー */
+	s_logBufferIndex += len - 1;							/* 終端 \0 分戻す */
+}
+static void writeLogFile(const char *fileName) {
+	FILE *fp = fopen(fileName, "wb");
+	fwrite(s_logBuffer, 1, strlen(s_logBuffer), fp);		/* 終端 \0 は書き込まれない */
+	fclose(fp);
+}
+
+/* メモリ確保した領域にファイルを読み込む */
+void *mallocReadFile(
+	const char *fileName,
+	size_t *sizeRet
+){
+	FILE *fd = fopen(fileName, "rb");
+	if (fd == NULL) return NULL;
+	struct stat stbuf;
+	if (fstat(fileno(fd), &stbuf) == -1) {
+		fclose(fd);
+		return NULL;
+	}
+	size_t sizeInBytes = (uint32_t)stbuf.st_size;
+	void *buffer = malloc(sizeInBytes);
+	if (buffer == NULL) {
+		fclose(fd);
+		return NULL;
+	}
+	fread(buffer, 1, sizeInBytes, fd);
+	*sizeRet = sizeInBytes;
+	fclose(fd);
+	return buffer;
+}
+
+/* CRC32 を求める */
+uint32_t crc32(
+	const	void		*p,
+			size_t		sizeInBytes
+){
+	static const uint32_t crcTable[] = {
+		0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
+		0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
+		0x1db71064, 0x6ab020f2, 0xf3b97148, 0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+		0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec, 0x14015c4f, 0x63066cd9, 0xfa0f3d63, 0x8d080df5,
+                                                                                                      
+		0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172, 0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,
+		0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6, 0xacbcf940, 0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+		0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423, 0xcfba9599, 0xb8bda50f,
+		0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924, 0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,
+                                                                                                      
+		0x76dc4190, 0x01db7106, 0x98d220bc, 0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+		0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d, 0x91646c97, 0xe6635c01,
+		0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e, 0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457,
+		0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea, 0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+                                                                                                      
+		0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7, 0xa4d1c46d, 0xd3d6f4fb,
+		0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0, 0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9,
+		0x5005713c, 0x270241aa, 0xbe0b1010, 0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+		0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81, 0xb7bd5c3b, 0xc0ba6cad,
+                                                                                                      
+		0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a, 0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683,
+		0xe3630b12, 0x94643b84, 0x0d6d6a3e, 0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+		0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb, 0x196c3671, 0x6e6b06e7,
+		0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc, 0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5,
+                                                                                                      
+		0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4, 0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+		0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55, 0x316e8eef, 0x4669be79,
+		0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236, 0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f,
+		0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92, 0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+                                                                                                      
+		0x9b64c2b0, 0xec63f226, 0x756aa39c,  0x26d930a, 0x9c0906a9, 0xeb0e363f, 0x72076785, 0x05005713,
+		0x95bf4a82, 0xe2b87a14, 0x7bb12bae,  0xcb61b38, 0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21,
+		0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8, 0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+		0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69, 0x616bffd3, 0x166ccf45,
+                                                                                                      
+		0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2, 0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db,
+		0xaed16a4a, 0xd9d65adc, 0x40df0b66, 0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+		0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
+		0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94, 0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d,
+	};
+	uint32_t crc = 0xFFFFFFFF;
+	const uint8_t *pU8 = (const uint8_t *)p;
+	const uint8_t *pU8End = pU8 + sizeInBytes;
+	for (; pU8 < pU8End; pU8++) {
+		crc = crcTable[(uint8_t)crc ^ *pU8] ^ (crc >> 8);
+	}
+	return crc ^ 0xFFFFFFFF;
+}
 
 /* 内部時計の状態を取得（上位16bit=秒の1桁目、下位16bit=秒の2桁目）*/
 static FORCE_INLINE uint32_t
@@ -425,8 +528,7 @@ measurement(
 	DisplaySpBg displaySpBg,
 	TVramMode tvramMode,
 	GVramMode gvramMode,
-	MemcpyMethod memcpyMethod,
-	FILE *fp
+	MemcpyMethod memcpyMethod
 ){
 	char buff[0x1000];
 
@@ -538,7 +640,7 @@ measurement(
 				testCase,
 				numTestCases - 1
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		} break;
 		case CrtHf_31KHz: {
 			CRTMOD(12);
@@ -552,7 +654,7 @@ measurement(
 				testCase,
 				numTestCases - 1
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		} break;
 	}
 
@@ -598,7 +700,7 @@ measurement(
 				buff,
 				"SP BG disp off (by $EB0808)\n"
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		} break;
 		case DisplaySpBg_On: {
 			*(uint8_t *)0xEB0808 = *(uint8_t *)0xEB0808 |  2;
@@ -606,7 +708,7 @@ measurement(
 				buff,
 				"SP BG disp on (by $EB0808)\n"
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		} break;
 	}
 
@@ -664,7 +766,7 @@ measurement(
 			(tvramMode == TVramMode_Display)? "display": "buffer",
 			(gvramMode == GVramMode_Display)? "display": "buffer"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 	/* MemcpyMethod をログに記録 */
@@ -673,7 +775,7 @@ measurement(
 			buff,
 			s_memcpyMethodDescriptions[memcpyMethod]
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 	/* ログの header */
@@ -684,14 +786,14 @@ measurement(
 			"throughput (bytes/sec)\n"
 			"+------------------------+"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
 				buff,
 				"----------+"
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
@@ -699,7 +801,7 @@ measurement(
 			"\n"
 			"|                   dst  |"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
@@ -707,7 +809,7 @@ measurement(
 				"  %s  |",
 				s_dstMemInfoArray[iDst].name
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
@@ -715,7 +817,7 @@ measurement(
 			"\n"
 			"|                        |"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
@@ -723,7 +825,7 @@ measurement(
 				" 0x%06x |",
 				s_dstMemInfoArray[iDst].p
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
@@ -731,7 +833,7 @@ measurement(
 			"\n"
 			"|  src                   |"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
@@ -739,7 +841,7 @@ measurement(
 				"~0x%06x |",
 				s_dstMemInfoArray[iDst].p + MEM_BLOCK_SIZE_IN_BYTES - 1
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
@@ -747,21 +849,21 @@ measurement(
 			"\n"
 			"+------------------------+"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
 				buff,
 				"----------+"
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
 			buff,
 			"\n"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 	/* 現在時刻の初期値 */
@@ -789,7 +891,7 @@ measurement(
 			s_srcMemInfoArray[iSrc].p,
 			s_srcMemInfoArray[iSrc].p + MEM_BLOCK_SIZE_IN_BYTES - 1
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		/* 秒の頭を検出 */
 		{
@@ -897,7 +999,7 @@ measurement(
 
 			/* 何かキーが押されていたら中断 */
 			if (INPOUT(0xFF)) {
-				printf("aborted"); fprintf(fp, "aborted");
+				printLogFile("aborted");
 				return false;
 			}
 		}
@@ -918,30 +1020,7 @@ measurement(
 								s_dstMemInfoArray[iDst].name,
 								i
 							);
-							printf(buff); fprintf(fp, buff);
-
-							/* メモリブロックをログにダンプする */
-							fprintf(fp, "src:\n");
-							for (int i = 0; i < MEM_BLOCK_SIZE_IN_BYTES; ++i) {
-								fprintf(
-									fp,
-									"%02x%s",
-									u8Src[i],
-									(i & 15) == 15? "\n": " ",
-									i
-								);
-							}
-							fprintf(fp, "dst:\n");
-							for (int i = 0; i < MEM_BLOCK_SIZE_IN_BYTES; ++i) {
-								fprintf(
-									fp,
-									"%02x%s",
-									u8Dst[i],
-									(i & 15) == 15? "\n": " ",
-									i
-								);
-							}
-
+							printLogFile(buff);
 							return false;
 						}
 					}
@@ -956,7 +1035,7 @@ measurement(
 				" %8d |",
 				results[iDst]
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		/* ログの改行 */
@@ -964,7 +1043,7 @@ measurement(
 			buff,
 			"\n"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 	/* ログの footer */
@@ -973,32 +1052,45 @@ measurement(
 			buff,
 			"+------------------------+"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 
 		for (int iDst = 0; iDst < COUNT_OF(s_dstMemInfoArray); ++iDst) {
 			sprintf(
 				buff,
 				"----------+"
 			);
-			printf(buff); fprintf(fp, buff);
+			printLogFile(buff);
 		}
 
 		sprintf(
 			buff,
 			"\n\n"
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 	return true;
 }
 
-void main()
+int main(int argc, char *argv[])
 {
 	SUPER(0);
 
 	/* ヒープサイズを拡張 */
 	allmem();
+
+	/* 実行ファイルの crc32 を求める */
+	uint32_t crc = 0;
+	{
+		size_t sizeInBytes;
+		void *p = mallocReadFile(argv[0], &sizeInBytes);
+		if (p == NULL) {
+			printf("ERROR : can not read %s.\n", argv[0]);
+			return EXIT_FAILURE;
+		}
+		crc = crc32(p, sizeInBytes);
+		free(p);
+	}
 
 	/* 512x512 256color 2plane 31kHz に設定 */
 	CRTMOD(8);			/* X68000 EnvironmentHandBook p.296 */
@@ -1017,9 +1109,6 @@ void main()
 	/* カーソル消去 */
 	B_CUROFF();			/* X68000 EnvironmentHandBook p.312 */
 
-	/* ログファイルを開く */
-	FILE *fp = fopen("mem_bw.log", "wb");
-
 	/* ビルド設定を記録 */
 	{
 		char buff[0x1000];
@@ -1027,10 +1116,12 @@ void main()
 			buff,
 			"build settings\n"
 			"	ENABLE_SRAM_WRITE %d\n"
+			"	BUILD " __DATE__ ", CRC32 %08X\n"
 			"\n",
-			ENABLE_SRAM_WRITE
+			ENABLE_SRAM_WRITE,
+			crc
 		);
-		printf(buff); fprintf(fp, buff);
+		printLogFile(buff);
 	}
 
 #if ENABLE_SRAM_WRITE
@@ -1065,8 +1156,7 @@ void main()
 							displaySpBg,
 							tvramMode,
 							gvramMode,
-							memcpyMethod,
-							fp
+							memcpyMethod
 						);
 						if (ret == false) {
 							goto aborted;
@@ -1119,8 +1209,7 @@ void main()
 					displaySpBg,
 					tvramMode,
 					gvramMode,
-					memcpyMethod,
-					fp
+					memcpyMethod
 				);
 				if (ret == false) {
 					goto aborted;
@@ -1141,9 +1230,11 @@ aborted:
 	B_BPOKE((UBYTE *)0xE8E00D, 0x00);
 #endif
 
-	/* ログファイルを閉じる */
-	fclose(fp);
+	/* ログファイルを書き出す */
+	writeLogFile("mem_bw.log");
 
 	/* 画面モードを戻す */
 	CRTMOD(0x10);
+
+	return EXIT_SUCCESS;
 }
